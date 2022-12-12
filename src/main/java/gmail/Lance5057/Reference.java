75df88be8d45cbef7d7544e04b25d0193a86e5ef
glowredman <35727266+glowredman@users.noreply.github.com>
2022-12-12 22:57:16 +0100

    Update buildscript

--- src/main/java/gmail/Lance5057/Reference.java
+++ src/main/java/gmail/Lance5057/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	
 	public static final String MOD_ID = "tinkersdefense";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "GRADLETOKEN_VERSION";
 	public static final String MOD_NAME = "Tinkers' Defense";
 	
 }
