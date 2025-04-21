fd9d8e8121f7552d6e51c5d1ef718e8356c8854d
Maya <10861407+serenibyss@users.noreply.github.com>
2025-04-21 11:47:48 -0500

    update

--- src/main/java/gmail/Lance5057/Reference.java
+++ src/main/java/gmail/Lance5057/Reference.java
@@ -3,6 +3,6 @@ package gmail.Lance5057;
 public class Reference {
 
     public static final String MOD_ID = "tinkersdefense";
-    public static final String VERSION = "GRADLETOKEN_VERSION";
+    public static final String VERSION = TDTags.VERSION;
     public static final String MOD_NAME = "Tinkers' Defense";
 }
